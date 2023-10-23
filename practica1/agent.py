3f13dbd118c43abd74d1a4190261556f502329f3
KafeisM <jordifloritensenyat03@gmail.com>
2023-10-23 18:42:16 +0200

    estat

--- practica1/agent.py
+++ practica1/agent.py
@@ -10,9 +10,47 @@ from practica1 import joc
 from practica1.entorn import Accio
 
 class Estat:
+    def __init__(self, estat: list, pes: int, torn: bool, pare: None):
+        self.__info = estat
+        self.__pes = pes
+        self.__pare = pare
+        self.__torn = torn
 
-    def __init__(self):
+    def __hash__(self):
+        return hash(tuple(self.__info))
+
+    def es_meta(self) -> bool:
+        llista = self.__info
+
+        #mirar files
+        for fila in llista:
+            if '1111' in fila or '2222' in fila:
+                return True
+
+        #mirar columnes
+        for j in range(len(self.__info[0])):
+            columna = ''.join(llista[i][j] for i in range(len(llista)))
+            if '1111' in columna or '2222' in columna:
+                return True
+
+        #mirar columnes
+        for i in range(len(llista) -3):
+            for j in range(len(llista[i])-3):
+                diagonal1 = ''.join(llista[i+k][j+k] for k in range(4))
+                diagonal2 = ''.join(llista[i + k][j+3-k] for k in range(4))
+                if '1111' in diagonal1 or '2222' in diagonal1:
+                    return True
+                if '1111' in diagonal2 or '2222' in diagonal2:
+                    return True
+        return False
+
+    def genera_fills(self) -> list:
         pass
+
+
+    @staticmethod
+    def canvia_torn(self):
+        self.__torn = not self.__torn
 
 class Agent(joc.Agent):
     def __init__(self, nom):
@@ -22,4 +60,14 @@ class Agent(joc.Agent):
         pass
 
     def actua(self, percepcio: entorn.Percepcio) -> entorn.Accio | tuple[entorn.Accio, object]:
-        pass
+        inici = [
+            "10000",
+            "01000",
+            "00100",
+            "00020",
+            "00000"
+        ]
+
+        prova = Estat(inici,0,None)
+        print(prova.es_meta())
+        return
